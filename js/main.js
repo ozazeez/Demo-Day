23f5348fc0bb23d0cebbb81e6736d4173f5cf283
Azeez <azeezolaosebikan1@gmail.com>
2025-05-09 21:19:44 -0400

    added tank objects

---
+++ js/main.js
@@ -0,0 +1,44 @@
+let partslist = []
+let tanks = [smallTank, mediumTank, largeTank]
+let filtration = []
+let lighting = []
+let rocks = []
+let sand = []
+let stand = []
+let heater = []
+let saltmix = []
+let wavemaker = []
+const Filter =
+{
+    name: 'AquaClear 110',
+    description: 'Hang on back filter with ',
+    dimensions: 'L: 36.5 IN W: 18.5 IN H: 16 IN',
+    gph: '500 Gallons'
+}
+
+const smallTank =
+{
+    name: 'Aqueon 40 Breeder',
+    description: '40 Gallon Aquarium',
+    dimensions: 'L: 36.5 IN W: 18.5 IN H: 16 IN',
+    volume: '40 Gallons'
+}
+
+const mediumTank =
+{
+    name: 'Aqueon 75 Gallon',
+    description: '40 Gallon Aquarium',
+    dimensions: 'L: 48 IN W: 18 IN H: 23 IN',
+    volume: '75 Gallons'
+}
+
+const largeTank =
+{
+    name: 'Aqueon 125 Gallon',
+    description: '40 Gallon Aquarium',
+    dimensions: 'L: 72.5 IN W: 18.5 IN H: 23.5 IN',
+    volume: '125 Gallons'
+}
+
+
+
