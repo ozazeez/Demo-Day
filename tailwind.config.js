859a81913041dc409a48e624f88122f8ca8ac559
Azeez <azeezolaosebikan1@gmail.com>
2025-05-12 15:00:13 -0400

    added daisyui

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+// tailwind.config.js
+// module.exports = {
+//   content: ["./views/**/*.ejs", "./public/css/**/*.js"],
+//   theme: {
+//     extend: {
+//       colors: {
+//         main: {
+//           dark: '#0A2463',
+//           teal: '#2CCED2',
+//           coral: '#FF6B6B',
+//           light: '#F5F0E6',
+//           sea: '#88C9BF'
+//         }
+//       },
+//     },
+//   },
+//   plugins: [require("daisyui")],
+// }
